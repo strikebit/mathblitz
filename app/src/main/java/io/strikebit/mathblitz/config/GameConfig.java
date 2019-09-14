f8b323b5e1bdb77b4bd817de9ccaf9b257d09110
Aaron Benson <bensonan55@gmail.com>
2019-09-14 17:25:28 -0500

    Update launcher icon

--- app/src/main/java/io/strikebit/mathblitz/config/GameConfig.java
+++ app/src/main/java/io/strikebit/mathblitz/config/GameConfig.java
@@ -7,10 +7,10 @@ public class GameConfig {
 
     public static final int TIME_TRIAL_DURATION_SECONDS = 60;
 
-    public final static int DIFFICULTY_VERY_EASY = -1;
-    public final static int DIFFICULTY_EASY = 0;
-    public final static int DIFFICULTY_ADEPT = 1;
-    public final static int DIFFICULTY_HARD = 2;
-    public final static int DIFFICULTY_VERY_HARD = 3;
-    public final static int DIFFICULTY_LEGENDARY = 4;
+    public final static int DIFFICULTY_VERY_EASY = 0;
+    public final static int DIFFICULTY_EASY = 1;
+    public final static int DIFFICULTY_ADEPT = 2;
+    public final static int DIFFICULTY_HARD = 3;
+    public final static int DIFFICULTY_VERY_HARD = 4;
+    public final static int DIFFICULTY_LEGENDARY = 5;
 }
